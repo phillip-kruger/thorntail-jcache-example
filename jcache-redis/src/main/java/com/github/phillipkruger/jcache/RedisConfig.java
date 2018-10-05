f1104b2d15f6458b65087dfcf5eaed773b0eda2e
Phillip Kruger <phillip.kruger@gmail.com>
2018-10-05 15:04:08 +0200

    Added Redis (not working yet)

--- jcache-redis/src/main/java/com/github/phillipkruger/jcache/RedisConfig.java
+++ jcache-redis/src/main/java/com/github/phillipkruger/jcache/RedisConfig.java
@@ -2,6 +2,7 @@ package com.github.phillipkruger.jcache;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.logging.Level;
 import javax.cache.CacheManager;
 import javax.cache.Caching;
@@ -13,23 +14,18 @@ import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import lombok.Getter;
 import lombok.extern.java.Log;
-import org.apache.ignite.Ignite;
-import org.apache.ignite.Ignition;
-import org.apache.ignite.events.CacheEvent;
-import org.apache.ignite.events.EventType;
-import org.apache.ignite.lang.IgnitePredicate;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 /**
- * Configure Ignite
+ * Configure Redis
  * @author Phillip Kruger (phillip.kruger@phillip-kruger.com)
  * 
  */
 @Log
 @ApplicationScoped
-public class IgniteConfig {
+public class RedisConfig {
     
-    @Inject @ConfigProperty(name = "ignite.configuration.file", defaultValue = "ignite.xml")
+    @Inject @ConfigProperty(name = "redis.configuration.file", defaultValue = "redis.xml")
     private String configurationFileName;
     
     @Produces
@@ -39,7 +35,7 @@ public class IgniteConfig {
     
     public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
         
-        log.log(Level.INFO, " ### Apache Ignite started as JCache IMDG");    
+        log.log(Level.INFO, " ### Redis started as JCache IMDG");    
         CachingProvider cachingProvider = Caching.getCachingProvider();
         URI resource = getConfigFile(cachingProvider);
         manager = cachingProvider.getCacheManager(resource,getClass().getClassLoader());
@@ -47,35 +43,23 @@ public class IgniteConfig {
         for(String name:manager.getCacheNames()) {
             log.log(Level.INFO, " ###   Found cache [{0}]", name);
         }
-        
-        
-        
-        
-        Ignite ignite = Ignition.ignite();
-
-        // Local listener that listenes to local events.
-        IgnitePredicate<CacheEvent> locLsnr = evt -> {
-            log.log(Level.SEVERE, "Received event [evt={0}, key={1}, oldVal={2}, newVal={3}", new Object[]{evt.name(), evt.key(), evt.oldValue(), evt.newValue()});
-            return true; // Continue listening.
-        };
-
-        // Subscribe to specified cache events occuring on local node.
-        ignite.events().localListen(locLsnr,
-                EventType.EVT_CACHE_OBJECT_PUT,
-                EventType.EVT_CACHE_OBJECT_READ,
-                EventType.EVT_CACHE_OBJECT_REMOVED,
-                EventType.EVT_CACHE_OBJECT_EXPIRED);
           
     }
     
     private URI getConfigFile(CachingProvider cachingProvider){
         try {
             ClassLoader cl = getClass().getClassLoader();
-            URI resource = cl.getResource(configurationFileName).toURI();
-            log.log(Level.INFO, "Using [{0}] as configuration", resource);
-            return resource;
+            URL url = cl.getResource(configurationFileName);
+            if(url!=null){
+                URI resource = url.toURI();
+                log.log(Level.INFO, "Using [{0}] as configuration", resource);
+                return resource;
+            }else{
+                log.log(Level.WARNING, "No config file provided, using default");
+                return cachingProvider.getDefaultURI();
+            }
         }catch (URISyntaxException ex) {
-            log.log(Level.WARNING, "No config file provided, using default [" + ex.getMessage() + "]");
+            log.log(Level.WARNING, "No config file provided, using default [{0}]", ex.getMessage());
             return cachingProvider.getDefaultURI();
         }
     }
