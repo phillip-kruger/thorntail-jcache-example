d1adb8d90d757da9a0f880f11d3f3541c4b7739a
Phillip Kruger <phillip.kruger@gmail.com>
2018-10-07 20:17:39 +0200

    Added Redis (not working yet)

--- jcache-redis/src/main/java/com/github/phillipkruger/jcache/RedisConfig.java
+++ jcache-redis/src/main/java/com/github/phillipkruger/jcache/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.github.phillipkruger.jcache;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -8,6 +9,7 @@ import javax.cache.CacheManager;
 import javax.cache.Caching;
 import javax.cache.spi.CachingProvider;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Destroyed;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Produces;
@@ -15,6 +17,9 @@ import javax.inject.Inject;
 import lombok.Getter;
 import lombok.extern.java.Log;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.redisson.RedissonNode;
+import org.redisson.config.Config;
+import org.redisson.config.RedissonNodeConfig;
 
 /**
  * Configure Redis
@@ -25,7 +30,7 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 @ApplicationScoped
 public class RedisConfig {
     
-    @Inject @ConfigProperty(name = "redis.configuration.file", defaultValue = "redis.xml")
+    @Inject @ConfigProperty(name = "redis.configuration.file", defaultValue = "redis.yml")
     private String configurationFileName;
     
     @Produces
@@ -33,18 +38,37 @@ public class RedisConfig {
     @Getter
     private CacheManager manager;
     
+    private RedissonNode node = null;
+    
     public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
-        
-        log.log(Level.INFO, " ### Redis started as JCache IMDG");    
-        CachingProvider cachingProvider = Caching.getCachingProvider();
-        URI resource = getConfigFile(cachingProvider);
-        manager = cachingProvider.getCacheManager(resource,getClass().getClassLoader());
-
-        for(String name:manager.getCacheNames()) {
-            log.log(Level.INFO, " ###   Found cache [{0}]", name);
+        try {
+            CachingProvider cachingProvider = Caching.getCachingProvider();
+            URI resource = getConfigFile(cachingProvider);
+            
+            Config config = Config.fromYAML(resource.toURL());
+            
+            RedissonNodeConfig nodeConfig = new RedissonNodeConfig(config);
+            
+            node = RedissonNode.create(nodeConfig);
+            
+            node.start();
+            
+            log.log(Level.INFO, " ### Redis started as JCache IMDG");
+            manager = cachingProvider.getCacheManager(resource,getClass().getClassLoader());
+            
+            for(String name:manager.getCacheNames()) {
+                log.log(Level.INFO, " ###   Found cache [{0}]", name);
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
           
     }
+    
+    public void shutdown(@Observes @Destroyed(ApplicationScoped.class) Object init) {
+        if(node!=null)node.shutdown();
+    }
+    
     
     private URI getConfigFile(CachingProvider cachingProvider){
         try {
